a41cf78949a393a0b4f664fafdbaca3fc90cd07e
iigokingii <smolik.valera@mail.ru>
2023-05-16 02:33:23 +0300

    completed lab13 DB

---
+++ BD/Lab13/SQLQuery2.sql
@@ -0,0 +1,191 @@
+--lab13.2
+--#1
+GO
+	CREATE PROCEDURE PROC1
+	AS 
+	BEGIN
+		DECLARE @K INT = (SELECT COUNT(*) FROM ŒÚ‚ÂÚÒÚ‚ÂÌÌ˚È);
+		SELECT *FROM ŒÚ‚ÂÚÒÚ‚ÂÌÌ˚È
+		RETURN @K
+	END
+GO
+DECLARE @K INT = 0;
+EXEC @K = PROC1
+PRINT 'NUMBER OF ROWS: '+ CONVERT(NVARCHAR(20),@K);
+
+--#2
+GO
+	ALTER PROCEDURE PROC1
+	 @F NVARCHAR(30) =NULL,
+	 @O INT OUTPUT
+	  AS
+		BEGIN
+		  DECLARE @K INT = (SELECT COUNT(*) FROM ŒÚ‚ÂÚÒÚ‚ÂÌÌ˚È);
+		  SELECT *FROM ŒÚ‚ÂÚÒÚ‚ÂÌÌ˚È WHERE ‘‡ÏËÎËˇ_ŒÚ‚ÂÚÒÚ‚ÂÌÌÓ„Ó = @F
+		  SET @O = @@ROWCOUNT;
+		  RETURN @K;
+		END
+
+
+	GO
+	DECLARE @K INT=0,@RC INT =0, @P VARCHAR(20)='Ivanov'
+	EXEC @K = PROC1 @F=@P,@O=@RC OUTPUT;
+	PRINT ' ŒÀ»◊≈—“¬Œ —“–Œ : '+CAST (@RC AS VARCHAR(10))
+	PRINT 'Œ¡Ÿ≈≈  ŒÀ»◊≈—“¬Œ '+CAST (@K AS VARCHAR(10))
+	
+--#3
+	CREATE TABLE #TEMPOTV
+	(
+		ID INT PRIMARY KEY,
+		SURNAME NVARCHAR(30),
+		NAME NVARCHAR(30),
+		PATRONYMIC NVARCHAR(20),
+		POSITION NVARCHAR(30),
+		DATE DATE
+	)
+	GO
+		ALTER PROCEDURE PROC1
+			@F NVARCHAR(30) = NULL
+				AS
+				BEGIN
+					SELECT *FROM ŒÚ‚ÂÚÒÚ‚ÂÌÌ˚È WHERE ‘‡ÏËÎËˇ_ŒÚ‚ÂÚÒÚ‚ÂÌÌÓ„Ó = @F
+				END
+	GO
+		INSERT #TEMPOTV EXEC PROC1 @F='Ivanov'
+		SELECT*FROM #TEMPOTV
+
+--#4
+CREATE TABLE #TEMP_SPIS(
+		ID INT PRIMARY KEY,
+		NAME NVARCHAR(60),
+		CAUSE NVARCHAR(90),
+		DATE DATE
+	)
+	DROP TABLE #TEMP_SPIS
+
+	GO
+		CREATE PROCEDURE TEMP_SPIS_INSERT 
+			@A INT,
+			@N NVARCHAR(60),
+			@C NVARCHAR(90),
+			@T DATE
+		AS DECLARE @RC INT = 1;
+		BEGIN TRY
+		 INSERT INTO #TEMP_SPIS VALUES (@A,@N,@C,@T)
+		 RETURN @RC;
+		END TRY
+		BEGIN CATCH
+		 PRINT 'ERROR NUMBER: '+CAST(ERROR_NUMBER() AS VARCHAR(6));
+		 PRINT 'MESSAGE :' + ERROR_MESSAGE();
+		 PRINT '”Ó‚ÂÌ¸ :' + CAST(ERROR_SEVERITY() AS VARCHAR(6));
+		 PRINT 'Ã≈“ ¿ : ' + CAST(ERROR_STATE() AS VARCHAR(8));
+		 PRINT 'ÕŒÃ≈– —“–Œ » :'+CAST(ERROR_LINE() AS VARCHAR(8));
+		 IF(ERROR_PROCEDURE() IS NOT NULL)
+		  PRINT 'NAME OF PROCEDURE: ' + ERROR_PROCEDURE();
+		 RETURN -1;
+		END CATCH
+
+	GO
+		DECLARE @RC INT;
+		EXEC @RC = TEMP_SPIS_INSERT @A =5,@N = 'SAMSUNG',@C = 'Œ¡ÕŒ¬À≈Õ Õ¿ ¡ŒÀ≈≈ ÕŒ¬”ﬁ ÃŒƒ≈À‹', @T = '2009-12-2'
+		PRINT ' Œƒ Œÿ»¡ »:'+CAST(@RC AS VARCHAR(3));
+		SELECT *FROM #TEMP_SPIS 
+	
+	
+	
+	--#5
+	GO
+		CREATE PROCEDURE EQUIPMENT_REPORT
+		 @P CHAR(10)
+		 AS 
+			DECLARE @RC INT =0;
+			BEGIN TRY
+			 DECLARE @REPORT CHAR(300) = '',@TEMP CHAR(40);
+			 DECLARE CURS CURSOR FOR 
+				SELECT Õ‡Á‚‡ÌËÂ_Œ·ÓÛ‰Ó‚‡ÌËˇ FROM Œ·ÓÛ‰Ó‚‡ÌËÂ WHERE “ËÔ_Œ·ÓÛ‰Ó‚‡ÌËˇ = @P;
+				IF(NOT EXISTS (SELECT Õ‡Á‚‡ÌËÂ_Œ·ÓÛ‰Ó‚‡ÌËˇ FROM Œ·ÓÛ‰Ó‚‡ÌËÂ WHERE “ËÔ_Œ·ÓÛ‰Ó‚‡ÌËˇ = @P))
+					RAISERROR('Œÿ»¡ ¿',11,1) --ÒÂ¸ÂÁÌÓÒÚ¸
+				ELSE
+					OPEN CURS
+					FETCH CURS INTO @TEMP
+					WHILE @@FETCH_STATUS =0
+					BEGIN
+					  SET @REPORT = RTRIM(@TEMP)+', '+@REPORT;
+					  SET @RC +=1; 
+					  FETCH CURS INTO @TEMP
+					END;
+				PRINT '‚˚‚Â‰ÂÌ˚ Í‡ÚÍËÂ Ì‡Á‚‡ÌËˇ (ÔÓÎÂ SUBJECT) ËÁ Ú‡·ÎË-ˆ˚ SUBJECT ‚ Ó‰ÌÛ ÒÚÓÍÛ ˜ÂÂÁ Á‡ÔˇÚÛ˛: '+ @REPORT;
+				CLOSE CURS
+				RETURN @RC
+			END TRY
+			BEGIN CATCH 
+				PRINT 'Œÿ»¡ ¿ ¬ œ¿–¿Ã≈“–¿’';
+				IF(ERROR_PROCEDURE() IS NOT NULL)
+					PRINT '»Ãﬂ œ–Œ÷≈ƒ”–€: '+ERROR_PROCEDURE();
+				RETURN @RC;
+			END CATCH
+
+	GO
+	DECLARE @RC INT;
+	EXEC @RC = EQUIPMENT_REPORT @P = 'œËÌÚÂ'
+	PRINT 'NUMBER OF DISCIPLINES: '+CAST(@RC AS NVARCHAR(29));
+
+	--#6
+	CREATE TABLE #TEMP_OTV(
+		DATE2 DATE PRIMARY KEY,
+		DATE DATE,
+	)
+
+
+	GO
+	CREATE PROCEDURE PAUDITORIUM_INSERTX
+		@A INT,							--ID
+		@N NVARCHAR(60),		--»Ãﬂ Œ¡Œ–”ƒ
+		@C NVARCHAR(90),		--“»œ Œ¡Œ–
+		@T DATE,						--ƒ¿“¿ œŒ—“¿¬ »
+	  @TN DATE			--ƒ¿“¿ œ–»≈Ã¿ –¿¡Œ◊≈√Œ
+	  AS
+		DECLARE @RC INT = 1;
+		BEGIN TRY
+			SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+			BEGIN TRAN
+			  INSERT INTO #TEMP_OTV
+				VALUES (@T,@TN)
+			 EXEC @RC = TEMP_SPIS_INSERT @A =@A, @N=@N,@C=@C,@T=@T;
+			COMMIT TRAN;
+			RETURN @RC;
+		END TRY
+		BEGIN CATCH
+		 PRINT 'ERROR NUMBER: '+CAST(ERROR_NUMBER() AS VARCHAR(6));
+		 PRINT 'MESSAGE :' + ERROR_MESSAGE();
+		 PRINT '”Ó‚ÂÌ¸ :' + CAST(ERROR_SEVERITY() AS VARCHAR(6));
+		 PRINT 'Ã≈“ ¿ : ' + CAST(ERROR_STATE() AS VARCHAR(8));
+		 PRINT 'ÕŒÃ≈– —“–Œ » :'+CAST(ERROR_LINE() AS VARCHAR(8));
+		 IF(ERROR_PROCEDURE() IS NOT NULL)
+		  PRINT 'NAME OF PROCEDURE: ' + ERROR_PROCEDURE();
+		 IF (@@TRANCOUNT>0)
+		  ROLLBACK TRAN;
+		 RETURN -1;
+		END CATCH
+
+	GO
+		DECLARE @RC INT;
+		EXEC @RC = PAUDITORIUM_INSERTX @A	=0,@N='WTR.B3',@C = '—“¿ÕŒ ', @T = '2005-02-21',@TN = '2010-09-2'
+		PRINT '@RC = ' + CAST (@RC AS VARCHAR(3))
+	
+	SELECT * FROM #TEMP_OTV;
+	SELECT * FROM #TEMP_SPIS;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
